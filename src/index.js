3b82d02b7ebddc61f1c13a5b3a3dbb84de11d845
z3lovebaby <phu24012002@gmail.com>
2023-09-21 14:33:24 +0700

    insert prettier, lint-staged, huskyy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const hbs = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const route = require('./routes');
-app.use(express.static(path.join(__dirname, 'resources/public')))
+app.use(express.static(path.join(__dirname, 'resources/public')));
 //tu4.6 tich hop san
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //http logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 //template engine
-                app.engine('hbs', hbs.engine({ extname: '.hbs' }));
+app.engine('hbs', hbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 //set path
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -24,4 +26,4 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 // app.get('/news',(req,res)=>{res.render('home1');})
-app.listen(port, () => console.log(`Exa,ple at http://localhost:${port}`))
+app.listen(port, () => console.log(`Exa,ple at http://localhost:${port}`));
